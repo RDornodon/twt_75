b3a0f227414b96ab5fca2ae4638399a6ceea8cba
RDornodon <archelf.hun@gmail.com>
2021-10-13 16:15:49 +0200

    Initial commit

---
+++ old_sols.py
@@ -0,0 +1,8 @@
+#for t in[0]*int(input()):print(sum((t:=(l:=dict(zip('MDCLXVI',(1000,500,100,50,10,5,1)))[v],-l)[t>l])for v in input()[::-1]))
+
+# for t in[0]*int(input()):print(sum((t:=(l:=(1000,500,100,50,10,5,1)['MDCLXVI'.index(v)],-l)[t>l])for v in input()[::-1]))
+
+#for t in[0]*int(input()):print(sum((t:=((l:=5**((x:='IVXLCDM'.find(v))%2)*10**(x//2)),-l)[l<t])for v in input()[::-1]))
+
+#for t in[0]*int(input()):print(sum((t:=((l:=5**(x%2)*10**(x//2)),-l)[l<t])for x in map('IVXLCDM'.find,input()[::-1])))
+
