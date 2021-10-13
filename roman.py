dca009a3f9d781ff2b228acf0a992d3d21fe5ded
RDornodon <archelf.hun@gmail.com>
2021-10-13 17:47:14 +0200

    submitted one

--- roman.py
+++ roman.py
@@ -1,1 +1,1 @@
-for t in[0]*int(input()):print(sum((t:=((l:=5**(x%2)*10**(x//2)),-l)[l<t])for x in map('IVXLCDM'.find,input()[::-1])))
+for t in[0]*int(input()):print(sum(t:=((l:=5**(x%2)*10**(x//2)),-l)[l<t]for x in map('IVXLCDM'.find,input()[::-1])))
